0eea22dedec845822f08961352f23c97a122d561
ASToliarchuk <stolyarchukast@gmail.com>
2023-11-19 12:06:56 +0100

    +

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ btnLoadMore.classList.add('is-hidden');
 
 searchForm.addEventListener('submit', onSubmitForm);
 
-function onSubmitForm(event) {
+async function onSubmitForm(event) {
     event.preventDefault();
 
     gallery.innerHTML = '';
@@ -33,71 +33,71 @@ function onSubmitForm(event) {
         .split(' ')
         .join('+');
 
-
     if (keyOfSearchPhoto === '') {
         Notify.info('Enter your request, please!', paramsForNotify);
         return;
     }
 
-    fetchPhoto(keyOfSearchPhoto, page, perPage)
-        .then(data => {
-            const searchResults = data.hits;
-            if (data.totalHits === 0) {
-                Notify.failure('Sorry, there are no images matching your search query. Please try again.', paramsForNotify);
-            } else {
-                Notify.info(`Hooray! We found ${data.totalHits} images.`, paramsForNotify);
-          
-                createMarkup(searchResults);
-                lightbox.refresh();
+    try {
+        const data = await fetchPhoto(keyOfSearchPhoto, page, perPage);
+        const searchResults = data.hits;
 
-            };
-            if (data.totalHits > perPage) {
-                btnLoadMore.classList.remove('is-hidden');
-                window.addEventListener('scroll', showLoadMorePage);
-            };
-      
-        })
-        .catch(onFetchError);
+        if (data.totalHits === 0) {
+            Notify.failure('Sorry, there are no images matching your search query. Please try again.', paramsForNotify);
+        } else {
+             Notify.info(`Hooray! We found ${data.totalHits} images.`, paramsForNotify);
+            createMarkup(searchResults);
+            lightbox.refresh();
+        }
+
+        if (data.totalHits > perPage) {
+            btnLoadMore.classList.remove('is-hidden');
+        } else {
+            btnLoadMore.classList.add('is-hidden');
+        }
+    } catch (error) {
+        onFetchError();
+    }
 
     btnLoadMore.addEventListener('click', onClickLoadMore);
 
     event.currentTarget.reset();
-};
+}
 
-function onClickLoadMore() {
+async function onClickLoadMore() {
     page += 1;
-    fetchPhoto(keyOfSearchPhoto, page, perPage)
-        .then(data => {
-            const searchResults = data.hits;
-            const numberOfPage = Math.ceil(data.totalHits / perPage);
-            
-            createMarkup(searchResults);
-            if (page === numberOfPage) {
-                btnLoadMore.classList.add('is-hidden');
-                Notify.info("We're sorry, but you've reached the end of search results.", paramsForNotify);
-                btnLoadMore.removeEventListener('click', onClickLoadMore);
-                window.removeEventListener('scroll', showLoadMorePage);
-            };
-            lightbox.refresh();
- 
-        })
-        .catch(onFetchError);
-};
+
+    try {
+        const data = await fetchPhoto(keyOfSearchPhoto, page, perPage);
+        const searchResults = data.hits;
+        const numberOfPage = Math.ceil(data.totalHits / perPage);
+
+        createMarkup(searchResults);
+        if (page === numberOfPage) {
+            btnLoadMore.classList.add('is-hidden');
+            Notify.info("We're sorry, but you've reached the end of search results.", paramsForNotify);
+            btnLoadMore.removeEventListener('click', onClickLoadMore);
+        }
+        lightbox.refresh();
+    } catch (error) {
+        onFetchError();
+    }
+}
 
 function onFetchError() {
     Notify.failure('Oops! Something went wrong! Try reloading the page or make another choice!', paramsForNotify);
-};
-
-
-
-function showLoadMorePage() {
-    if (checkIfEndOfPage()) {
-        onClickLoadMore();
-    };
-};
-
-function checkIfEndOfPage() {
-  return (
-    window.innerHeight + window.scrollY >= document.documentElement.scrollHeight
-  );
 }
+
+
+
+// function showLoadMorePage() {
+//     if (checkIfEndOfPage()) {
+//         onClickLoadMore();
+//     };
+// };
+
+// function checkIfEndOfPage() {
+//   return (
+//     window.innerHeight + window.scrollY >= document.documentElement.scrollHeight
+//   );
+// }
